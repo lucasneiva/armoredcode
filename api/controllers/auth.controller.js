8d5492b4d83ab73708994e5649029b9b920c651d
Lucas Neiva <112989505+lucasneiva@users.noreply.github.com>
2024-03-06 17:38:45 -0300

    token

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,13 @@
 import Role from "../models/Role.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
 import { CreateError } from "../utils/error.js";
 import { CreateSuccess } from "../utils/success.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next)=>{
-    const role = await Role.find({role: 'User'});
+    const role = await Role.find({role: 'user'});
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
@@ -24,7 +26,8 @@ export const register = async (req, res, next)=>{
 export const login = async (req, res, next)=>{
 
     try {
-        const user = await User.findOne({email: req.body.email});
+        const user = await User.findOne({email: req.body.email})
+        .populate("roles", "role");
 
         if(!user) {
             return res.status(404).send("User not found!");
@@ -36,7 +39,23 @@ export const login = async (req, res, next)=>{
             return res.status(400).send("Password is incorrect!");
         }
 
-        return next(CreateSuccess(500, "deu bom", null));
+        
+        const token = jwt.sign(
+            {id: user._id, isAdmin: user.isAdmin, roles: user.roles},
+            process.env.JWT_SECRET
+        );
+        
+        
+        res.cookie("acess_token", token, {httpOnly: true})
+        .status(200)
+        .json({
+            status: 200,
+            message: "Login Success",
+            data: user
+        });
+        
+
+        //return next(CreateSuccess(200, "Login Success!"));
     } catch (error) {
         return next(CreateError(500, "deu ruim"));
     }
