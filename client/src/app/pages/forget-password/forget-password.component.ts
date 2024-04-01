2e7d2a9afb488b1d793e03af7e0895c5a1788bc4
arthur motter <arthur.motter.fatec@gmail.com>
2024-04-01 16:29:59 -0300

    tela de reset pass

--- client/src/app/pages/forget-password/forget-password.component.ts
+++ client/src/app/pages/forget-password/forget-password.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.scss'
 })
 export default class ForgetPasswordComponent {
+  fb = inject(FormBuilder);
 
+  forgetForm !: FormGroup;
+
+  ngOnInit(): void {
+    this.forgetForm = this.fb.group({
+      email: ['',Validators.compose ([Validators.required, Validators.email])]
+    })
+  }
+
+  submit(){
+    console.log(this.forgetForm.value);
+  }
 }
