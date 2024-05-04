fd457574967b89faddcefa00c81e1b598b793790
Lucas Neiva <112989505+lucasneiva@users.noreply.github.com>
2024-05-04 12:16:10 -0300

    client profile

---
+++ api/models/ClientProfile.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+import Location from './Location';
+
+const ClientProfileSchema = new Schema({
+    companyName: {
+        type: String,
+        required: true
+    },
+
+    companySize: {
+        type: String,
+        enum: ["SMALL", "MEDIUM", "LARGE"],
+        required: true
+    },
+
+    industry: {
+        type: String,
+        required: true
+    },
+
+    companyDescription: {
+        type: String
+    },
+
+    website: {
+        type: String
+    },
+
+    logo: {
+        type: String
+    },
+
+    location: {
+        type: Location.schema,
+    }
+
+});
+
+export default mongoose.model("ClientProfile", ClientProfileSchema); 
