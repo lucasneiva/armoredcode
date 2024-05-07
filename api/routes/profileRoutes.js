b9f863f53079c723af3ef43b25a5a85f373a177f
Lucas Neiva <112989505+lucasneiva@users.noreply.github.com>
2024-05-07 16:57:42 -0300

    fix profile auth

--- api/routes/profileRoutes.js
+++ api/routes/profileRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { createProfile } from "../controllers/profileController.js";
-import { verifyUser } from "../utils/authMiddleware.js";
+import { verifyToken } from "../utils/authMiddleware.js";
 
 const router = express.Router();
 
-router.post( "/", verifyUser, createProfile );
+router.post( "/", verifyToken, createProfile );
 
 export default router;
