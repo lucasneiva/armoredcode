144e1eaff74cdfae799b4c921ed74d71481a2b85
EdsonMYY <edsonmyy01@gmail.com>
2024-05-10 18:27:49 -0300

    Tela que o Mota criou

---
+++ client/src/app/pages/create-profile/create-profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-create-profile',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './create-profile.component.html',
+  styleUrl: './create-profile.component.scss'
+})
+export default class CreateProfileComponent {
+
+}
