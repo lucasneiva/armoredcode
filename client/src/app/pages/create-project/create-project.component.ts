a823fd06629dc918fdbfe2d14bfdf9cbad8ceb19
arthur motter <arthur.motter.fatec@gmail.com>
2024-05-10 17:46:40 -0300

    skills e botoes de projeto

--- client/src/app/pages/create-project/create-project.component.ts
+++ client/src/app/pages/create-project/create-project.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-create-project',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './create-project.component.html',
   styleUrl: './create-project.component.scss'
 })
