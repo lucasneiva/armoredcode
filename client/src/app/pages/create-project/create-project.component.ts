358cd2ccf5e1da595a75ebe5fdc08df5d131531f
arthur motter <arthur.motter.fatec@gmail.com>
2024-05-11 14:16:05 -0300

    create project input adds

--- client/src/app/pages/create-project/create-project.component.ts
+++ client/src/app/pages/create-project/create-project.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-create-project',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './create-project.component.html',
   styleUrl: './create-project.component.scss'
 })
-export default class CreateProjectComponent {
-  
+export default class CreateProjectComponent implements OnInit{
+  fb = inject(FormBuilder);
+  router = inject(Router);
+  createProjectForm !: FormGroup;
+
+  ngOnInit(): void {
+    this.createProjectForm = this.fb.group({
+      clientId: ['',Validators.required],
+      freelancerId: [''],
+      projectCategoryId: ['',Validators.required],
+      skillIds: [''],
+      projectTitle: ['',Validators.required],
+      projectDescription: ['',Validators.required],
+      projectHourlyRate: [''],
+      projectBudget: [''],
+      pricingType: ['',Validators.required],
+      estimatedDuration: ['',Validators.required],
+      projectSize: [''],
+      projectStatus: [''],
+      experienceLevel: [''],
+      workModel: ['',Validators.required],
+      location: [''],
+      startDate: [''],
+      endDate: [''],
+    },
+    );
+  }
 }
