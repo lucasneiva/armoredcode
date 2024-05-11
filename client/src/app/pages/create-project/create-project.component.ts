69b1d89bca499baf84ac53fc2dd403dc0b22c9d7
arthur motter <arthur.motter.fatec@gmail.com>
2024-05-11 09:15:20 -0300

    some fix

--- client/src/app/pages/create-project/create-project.component.ts
+++ client/src/app/pages/create-project/create-project.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+
 @Component({
   selector: 'app-create-project',
   standalone: true,
@@ -9,5 +10,5 @@ import { RouterModule } from '@angular/router';
   styleUrl: './create-project.component.scss'
 })
 export default class CreateProjectComponent {
-
+  
 }
