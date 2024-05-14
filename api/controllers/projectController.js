2d5560a3a007ffaa9d0c53c0e51cbdd0549987e9
Lucas Neiva <112989505+lucasneiva@users.noreply.github.com>
2024-05-14 16:27:25 -0300

    fix indent

--- api/controllers/projectController.js
+++ api/controllers/projectController.js
@@ -7,48 +7,44 @@ import projectJoiSchema from "../validators/projectValidator.js";
 import { validateData } from "../utils/validateData.js";
 import { connectToDatabase } from "../db.js";
 
-export const searchProjects = async (req, res, next) => {
-  try {
-    const db = await connectToDatabase();
-    const collection = db.collection('projects');
+export const searchProjects = async ( req, res, next ) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection( 'projects' );
 
-    const searchTerm = req.query.q; // Get search term from query parameter
+        const searchTerm = req.query.q; // Get search term from query parameter
 
-    const results = await collection.aggregate([
-      { 
-        $search: {
-          index: 'projects', // Replace 'default' with your index name if needed
-          text: {
-            query: searchTerm,
-            path: ['projectTitle', 'projectDescription'] // Fields to search
-          }
-        }
-      }
-    ]).toArray();
+        const results = await collection.aggregate( [
+            {
+                $search: {
+                    index: 'projects', // Replace 'default' with your index name if needed
+                    text: {
+                        query: searchTerm,
+                        path: [ 'projectTitle', 'projectDescription' ] // Fields to search
+                    }
+                }
+            }
+        ] ).toArray();
 
-    return next(CreateSuccess(200, 'Search Results', results));
+        return next( CreateSuccess( 200, 'Search Results', results ) );
 
-  } catch (error) {
-    return next(CreateError(500, 'Internal Server Error'));
-  }
+    } catch ( error ) {
+        return next( CreateError( 500, 'Internal Server Error' ) );
+    }
 };
 
 
 export const getProjectById = async ( req, res, next ) => {
-    
+
 };
 
 export const createProject = async ( req, res, next ) => {
     try {
         const token = req.cookies.acess_token;
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const decodedToken = jwt.verify( token, process.env.JWT_SECRET );
 
         const newProjectData = req.body;
-        
-        newProjectData.clientId = decodedToken.id; 
-
-        if ( decodedToken.id != newProjectData.clientId )
-            return next ( CreateError(400, "Not Authorizead!"));
+        newProjectData.clientId = decodedToken.id;
 
         /*
         try {
@@ -57,7 +53,7 @@ export const createProject = async ( req, res, next ) => {
             return next( CreateError( "400", error.message ));
         }
         */
-        
+
         const newproject = new project( newProjectData );
 
         await newproject.save();
