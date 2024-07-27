3ffc500f1ba4112c54dde892c2dadd3085719cfb
arthur motter <arthur.motter.fatec@gmail.com>
2024-07-27 14:54:32 -0300

    freelancer profile tests

--- client/src/app/pages/create-profile/create-profile.component.ts
+++ client/src/app/pages/create-profile/create-profile.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-create-profile',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './create-profile.component.html',
   styleUrl: './create-profile.component.scss'
 })
 export default class CreateProfileComponent {
+  fb = inject(FormBuilder);
+  authService = inject(AuthService);
+  router = inject(Router);
 
+  profileForm !: FormGroup;
+
+  isClient: boolean = false; //false is default
+
+  ngOnInit(): void {
+    this.profileForm = this.fb.group({
+
+    },
+    );
+  }
 }
