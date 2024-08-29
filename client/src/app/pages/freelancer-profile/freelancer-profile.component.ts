87590e9e3ac7ce9f7772ddd3ffe840357888dadd
Gustavo Mota <gustavo.mota01@fatec.sp.gov.br>
2024-08-29 11:13:45 -0300

    build ruim 29/08

---
+++ client/src/app/pages/freelancer-profile/freelancer-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-freelancer-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './freelancer-profile.component.html',
+  styleUrl: './freelancer-profile.component.scss'
+})
+export class FreelancerProfileComponent {
+
+}
