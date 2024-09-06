a3d197d64746e1aa90104f2e7705a9504eed390d
arthur motter <arthur.motter.fatec@gmail.com>
2024-09-06 14:48:57 -0300

    manage-project diff

--- client/src/app/pages/manage-project/manage-project.component.ts
+++ client/src/app/pages/manage-project/manage-project.component.ts
@@ -3,6 +3,7 @@ import { Router, RouterModule } from '@angular/router';
 import { ProjectCardComponent } from '../../components/project-card/project-card.component';
 import { Project, ProjectService } from '../../services/project.service';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-manage-project',
@@ -13,10 +14,24 @@ import { CommonModule } from '@angular/common';
 })
 export default class ManageProjectComponent implements OnInit{
   router = inject(Router);
+  authService = inject(AuthService);
   projectService = inject(ProjectService);
   projects: Project[] = [];// Array to store projects
 
+  userRole: string | null = null; 
+  isClient: boolean = false; 
+
   ngOnInit(): void {
+    this.userRole = this.authService.getUserRole();
+
+    if (this.userRole === 'CLIENT') {
+      this.isClient = true;
+    } else if (this.userRole === 'FREELANCER') {
+      this.isClient = false;
+    } else {
+      console.log("invalid role");
+    }
+
     this.projectService.getProjects().subscribe({
       next: (res) => {
         this.projects = res.data;
