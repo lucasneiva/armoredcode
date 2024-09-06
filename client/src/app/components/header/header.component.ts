a47b735427bb608be232d5de2b23481e2c4095e3
arthur motter <arthur.motter.fatec@gmail.com>
2024-09-06 14:36:41 -0300

    navbar redesign

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -7,9 +7,6 @@ import { AuthService } from '../../services/auth.service';
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, RouterModule],
-  template: `
-    <span *ngIf="isVisible">This text will disappear at certain resolutions.</span>
-  `,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -20,23 +17,9 @@ export class HeaderComponent implements OnInit {
   logoPath = '../../assets/images/logo_branca_sfundo.png';
   menuPath = '../../assets/images/menu-svgrepo-com.svg';
 
-  isVisible = true;
   showMenu: boolean = false;
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    this.isVisible = this.checkResolution();
-  }
-
-  checkResolution(): boolean {
-    const targetWidth = 1150;
-    // const targetHeight = ???; 
-
-    return window.innerWidth > targetWidth;
-  }
-
   ngOnInit(): void {
-    this.isVisible = this.checkResolution();
     this.authService.isLoggedIn$.subscribe(res => {
       this.isLoggedIn = this.authService.isLoggedIn();
     });
