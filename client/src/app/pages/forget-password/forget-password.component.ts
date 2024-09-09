c60cae7fd1944cd6b570b60637e2ef7dd9ee75b9
arthur motter <arthur.motter.fatec@gmail.com>
2024-09-09 08:50:26 -0300

    forget pass fixed

--- client/src/app/pages/forget-password/forget-password.component.ts
+++ client/src/app/pages/forget-password/forget-password.component.ts
@@ -1,17 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.scss'
 })
 export default class ForgetPasswordComponent {
   fb = inject(FormBuilder);
+  router = inject(Router);
   authService = inject(AuthService);
   forgetForm !: FormGroup;
 
