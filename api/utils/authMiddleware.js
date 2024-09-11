610fe5b17eeb2a30e1efa1251c95e60cf2130782
arthur motter <arthur.motter.fatec@gmail.com>
2024-09-11 09:58:08 -0300

    minor fixes

--- api/utils/authMiddleware.js
+++ api/utils/authMiddleware.js
@@ -17,8 +17,7 @@ export const verifyToken = ( req, res, next ) => {
     jwt.verify( token, process.env.JWT_SECRET, ( err, user ) => {
         if ( err )
             return next( createError( 403, "Invalid Token!" ) );
-        
-        console.log( "here" );
+        //modified
         req.user = user;
         next();
 
