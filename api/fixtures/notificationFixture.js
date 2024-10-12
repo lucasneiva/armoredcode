425b1d08ebe3c069f509bb98e4e17427c5bb7dfa
Lucas Neiva <112989505+lucasneiva@users.noreply.github.com>
2024-10-12 11:44:20 -0300

    Profile Id to UserId fix

--- api/fixtures/notificationFixture.js
+++ api/fixtures/notificationFixture.js
@@ -1,35 +1,35 @@
 import Notification from "../models/notificationModel.js";
-import FreelancerProfile from "../models/freelancerProfileModel.js";
-import Project from "../models/projectModel.js"; // Import Project model
+import User from "../models/userModel.js";
+import Project from "../models/projectModel.js";
 
 const seedNotifications = async () => {
     try {
-        const freelancerProfiles = await FreelancerProfile.find({}).lean();
-        const projects = await Project.find({}).lean(); // Fetch projects
+        const freelancerProfiles = await User.find( { role: "FREELANCER" } ).lean();
+        const projects = await Project.find( {} ).lean();
         const notificationData = [];
 
-        for (let i = 0; i < 20; i++) {
-            const randomFreelancer = freelancerProfiles[Math.floor(Math.random() * freelancerProfiles.length)];
+        for ( let i = 0; i < 25; i++ ) {
+            const randomFreelancer = freelancerProfiles[ Math.floor( Math.random() * freelancerProfiles.length ) ];
             const randomFreelancerId = randomFreelancer._id;
-            const randomProject = projects[Math.floor(Math.random() * projects.length)]; // Get a random project
-            const clientIdFromProject = randomProject.clientId; // Extract client ID
+            const randomProject = projects[ Math.floor( Math.random() * projects.length ) ];
+            const clientIdFromProject = randomProject.clientId;
 
-            notificationData.push({
+            notificationData.push( {
                 freelancerProfileId: randomFreelancerId,
-                clientId: clientIdFromProject, // Use client ID from project
-                projectId: randomProject._id, // You might want to randomize this too
+                clientId: clientIdFromProject,
+                projectId: randomProject._id, 
                 message: "Com a mais elevada consideração, tomo a liberdade de dirigir-me a Vossa Senhoria para formular um convite à participação em um projeto que, por certo, se avolumará com o fulgor de vossa notável sapiência e talento.",
                 read: false,
                 createdAt: new Date(),
                 updatedAt: new Date(),
-            });
+            } );
         }
 
-        await Notification.deleteMany({});
-        await Notification.insertMany(notificationData);
-        console.log("Notification data seeded successfully!");
-    } catch (error) {
-        console.error("Error seeding Notification data:", error);
+        await Notification.deleteMany( {} );
+        await Notification.insertMany( notificationData );
+        console.log( "Notification data seeded successfully!" );
+    } catch ( error ) {
+        console.error( "Error seeding Notification data:", error );
     }
 };
 
