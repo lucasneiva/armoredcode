3c7a0b60e6ead384cc43a4383b26f5c0ae9f6f0e
arthur motter <arthur.motter.fatec@gmail.com>
2024-11-17 13:43:48 -0300

    test 3

--- client/src/app/pages/manage-profile/manage-profile.component.ts
+++ client/src/app/pages/manage-profile/manage-profile.component.ts
@@ -33,6 +33,7 @@ export default class ManageProfileComponent implements OnInit {
   industry: Industry | null = null;
   specialization: Specialization | null = null;
   skills: string[] = [];
+  skillImages: { [key: string]: string } = {};
 
   isLoading = true;
   showConfirmationModal = false; // Flag for the confirmation modal
@@ -101,19 +102,25 @@ export default class ManageProfileComponent implements OnInit {
       });
   }
 
-  loadSkills(skillIds: any[] ) {
+  loadSkills(skillIds: any[]) {
     skillIds.forEach((skillId) => {
       this.skillService.getSkillById(skillId)
-      .subscribe({
-        next: (skillData) => {
-          this.skills.push(skillData.data.skillName);
-          /*debug*/ //console.log("skill: " + this.skills); console.log("id: " + skillId);
-        },
-        error: (error) => {
-          console.error("Error loading skill:", error);
-        }
-      });
+        .subscribe({
+          next: (skillData) => {
+            const skillName = skillData.data.skillName;
+            this.skills.push(skillName);
+            this.skillImages[skillName] = skillData.data.skillImage; // Store the image URL
+          },
+          error: (error) => {
+            console.error("Error loading skill:", error);
+          }
+        });
     });
+  }
+
+  getImageUrl(relativePath: string | undefined): string | undefined {
+    if (!relativePath) return undefined; // Handle cases where there's no image
+    return `${window.location.origin}/${relativePath}`;
   }
 
   // Function to show the confirmation modal
