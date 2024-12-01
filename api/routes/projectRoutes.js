460a1b7ed8e41a80ee9e7beaabd415457429c630
arthur motter <arthur.motter.fatec@gmail.com>
2024-12-01 09:16:37 -0300

    Revert "testing projects"
    This reverts commit 91f82b6191726de94d21c60357299ac6d57b19ea.

--- api/routes/projectRoutes.js
+++ api/routes/projectRoutes.js
@@ -4,26 +4,27 @@ import { verifyClient, verifyToken } from '../utils/authMiddleware.js';
 import { createRating, getProjectRatings, checkRatingCompletion  } from '../controllers/ratingController.js';
 
 const router = express.Router();
-//Project Routes
+
 router.get('/user', verifyToken, getUserProjects);
 
-router.get('/:id', verifyToken, getProjectById );
+router.get( '/:id', verifyToken, getProjectById );
 
-router.post('/', verifyClient, createProject );
+router.post( '/', verifyClient, createProject );
 
 router.patch('/:id', verifyToken, updateProject);
 
 router.delete('/:id', verifyClient, deleteProject); 
 
-router.get('/posted', getPostedProjects );
+router.get( '/', getPostedProjects );
 
-router.get('/user/posted', verifyToken, getPostedUserProjects); 
+router.get('/user/posted', verifyToken, getPostedUserProjects); // New route
+
 // Rating Routes
 router.post('/ratings', verifyToken, createRating);
 
 router.get('/projects/:projectId/ratings', getProjectRatings);
 
-router.get('/projects/:projectId/ratings/complete', checkRatingCompletion);
+router.get('/projects/:projectId/ratings/complete', checkRatingCompletion); // Use the controller function
 
 
 export default router;
